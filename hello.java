2914688589eac2369bfe8305e76cbbfd51a567e8
NIKHILKUMAR-1999 <nk7428089895@gmail.com>
2022-02-03 16:55:47 +0530

    Files added

---
+++ hello.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Hello {
+    public static void main(String[] args) {
+        int num;
+        Scanner sc = new Scanner(System.in);
+        num = sc.nextInt();
+        System.out.println(num);
+    }
+}
